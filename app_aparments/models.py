28876589bca468b1f42abe9aab63b03e7d1f1255
tomas12py <tomas.martinez@polygon.us>
2025-05-20 23:17:21 -0500

    The model apartament and abstract model timestamped were created

--- app_aparments/models.py
+++ app_aparments/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class TimeStamped(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta():
+        abstract = True
+
+class Apartment(TimeStamped):
+    title = models.CharField(max_length=150,blank=False)
+    price = models.IntegerField(blank=False)
+    rooms = models.IntegerField(blank=False)
+    bathrooms = models.IntegerField(blank=False)
+    address = models.CharField(max_length=100,blank=False)
+    location = models.CharField(max_length=50,blank=False)
+    square_meters = models.FloatField(blank=False)
+    images = models.ImageField(blank=False)
+    description = models.CharField(max_length=300,blank=False)
+
+    class Meta():
+        db_table = "apartment"
+
