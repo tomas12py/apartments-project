4b3a4e0fc087f68c59fda2131fd1fc2b839bcbdb
tomas12py <tomas.martinez@polygon.us>
2025-05-21 23:41:58 -0500

    The serializer was created

---
+++ app_aparments/serializer.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from .models import Apartment
+
+class ApartmentSerializer(serializers.ModelSerializer):
+
+    class Meta():
+        model = Apartment
+        fields = '__all__'
