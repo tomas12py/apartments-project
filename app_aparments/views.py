9a1635684ca6ee3aa511f22173122b083ada4c0a
tomas12py <tomas.martinez@polygon.us>
2025-05-21 23:43:15 -0500

    The new views were created

--- app_aparments/views.py
+++ app_aparments/views.py
@@ -1,6 +1,19 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
+from .serializer import ApartmentSerializer
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import status
+from .models import Apartment
 
-def test(request):
-    return HttpResponse("<h1>Hola</h1>")
+class GetAllAparments(APIView):
+    def get (self,request):
+        apartments = Apartment.objects.all()
+        apartments_serializer = ApartmentSerializer(apartments,many = True)
+        return Response(apartments_serializer.data)
+    
+class CreateAparment(APIView):
+    def post(self,request):
+        serializer = ApartmentSerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response("The aparment was created",status = status.HTTP_201_CREATED)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
