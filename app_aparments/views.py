2522ae1a7ad6722e8440e456e9723179ebf55fa0
tomas12py <tomas.martinez@polygon.us>
2025-05-23 23:17:03 -0500

    New views were created

--- app_aparments/views.py
+++ app_aparments/views.py
@@ -1,4 +1,5 @@
 from .serializer import ApartmentSerializer
+from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
@@ -17,3 +18,24 @@ class CreateAparment(APIView):
             serializer.save()
             return Response("The aparment was created",status = status.HTTP_201_CREATED)
         return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+    
+class GetAparmentById(APIView):
+    def get(self,request,id):
+        aparment = get_object_or_404(Apartment,pk = id)
+        serializer = ApartmentSerializer(aparment)
+        return Response(serializer.data)
+    
+class EditAparmentById(APIView):
+    def put(self,request,id):
+        aparment = get_object_or_404(Apartment,pk = id)
+        serializer = ApartmentSerializer(aparment,data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response("The aparment was modified",status=status.HTTP_200_OK)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+    
+class DeleteAparmentById(APIView):
+    def delete(self,request,id):
+        apartment = get_object_or_404(Apartment,pk = id)
+        apartment.delete()
+        return Response("The aparment was eliminated",status=status.HTTP_204_NO_CONTENT)
