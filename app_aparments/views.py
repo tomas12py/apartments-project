74cb694199577fc2c04eb2010d5bb0f136d247a4
tomas12py <tomas.martinez@polygon.us>
2025-05-27 23:18:51 -0500

    The text is convert to to capitalize case

--- app_aparments/views.py
+++ app_aparments/views.py
@@ -35,6 +35,7 @@ class AparmentGeneralMethods(APIView):
     def post(self,request):
         serializer = ApartmentSerializer(data = request.data)
         if serializer.is_valid():
+            serializer.validated_data["title"] = serializer.validated_data["title"].capitalize()
             serializer.save()
             return Response("The aparment was created",status = status.HTTP_201_CREATED)
         return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
