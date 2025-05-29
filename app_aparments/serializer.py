99c682da80cbba00a7407805f12dd965fa8793d6
tomas12py <tomas.martinez@polygon.us>
2025-05-28 23:38:12 -0500

    New serializer was created

--- app_aparments/serializer.py
+++ app_aparments/serializer.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from rest_framework import serializers
 from .models import Apartment
 
@@ -6,3 +7,18 @@ class ApartmentSerializer(serializers.ModelSerializer):
     class Meta():
         model = Apartment
         fields = '__all__'
+
+
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    class Meta():
+        model = User
+        fields  = ('username','password')
+
+    def create(self,validated_data):
+        user  = User(
+            username = validated_data["username"],
+            password = validated_data["password"]
+        )
+        user.set_password(validated_data["password"])
+        user.save()
+        return user
