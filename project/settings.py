bf3dbb9b7385fa3e616f237ac595e9e32b39d2ff
tomas12py <tomas.martinez@polygon.us>
2025-05-30 23:47:33 -0500

    Unnecessary print was removed

--- project/settings.py
+++ project/settings.py
@@ -16,7 +16,6 @@ from pathlib import Path
 import os
 
 config = dotenv_values(".env")
-print(config)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
