624ef651e0a3c2e823c761729eea7e72286eb8eb
tomas12py <tomas.martinez@polygon.us>
2025-05-30 23:46:02 -0500

    The routes were modified

--- project/urls.py
+++ project/urls.py
@@ -26,6 +26,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("aparment",AparmentGeneralMethods.as_view()),
     path("aparment/<int:id>/",AparmentMethodsById.as_view()),
-    path("register/",UserRegister.as_view()),
+    path("user/",UserRegister.as_view()),
+    path("user/<int:id>/",UserRegister.as_view()),
     path("login/",TokenObtainPairView.as_view())
 ]
