bea2da64d17406b30d67869882e422c9588853b6
tomas12py <tomas.martinez@polygon.us>
2025-06-03 23:39:00 -0500

    The settings for documentation was installed

--- project/settings.py
+++ project/settings.py
@@ -43,7 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'app_aparments',
-    'rest_framework'
+    'rest_framework',
+    'drf_spectacular'
 ]
 
 
@@ -137,6 +138,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
+    'DEFAULT_SCHEMA_CLASS':"drf_spectacular.openapi.AutoSchema"
 }
 
 
