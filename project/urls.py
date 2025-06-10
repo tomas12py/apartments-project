76585129f6c2bc5bc10e23a13a110367690ff061
tomas12py <tomas.martinez@polygon.us>
2025-06-09 23:42:19 -0500

    A new endpoint was created

--- project/urls.py
+++ project/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 
 from drf_spectacular.views import SpectacularAPIView,SpectacularRedocView,SpectacularSwaggerView
-from app_aparments.views import AparmentGeneralMethods,AparmentMethodsById,UserRegister
+from app_aparments.views import AparmentGeneralMethods,AparmentMethodsById,UserRegister,AparmentFiltering
 from rest_framework_simplejwt.views import TokenObtainPairView,TokenRefreshView
 from django.urls import include, path
 from django.contrib import admin
@@ -32,5 +32,6 @@ urlpatterns = [
     path("login/",TokenObtainPairView.as_view()),
     path("api/schema",SpectacularAPIView.as_view(),name = 'schema'),
     path("api/schema/swagger-ui/",SpectacularSwaggerView.as_view(url_name = 'schema'),name = 'swagger-ui'),
-    path("api/schema/redoc/",SpectacularRedocView.as_view(url_name = 'schema'),name = "redoc")
+    path("api/schema/redoc/",SpectacularRedocView.as_view(url_name = 'schema'),name = "redoc"),
+    path("aparment/",AparmentFiltering.as_view())
 ]
