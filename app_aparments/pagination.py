cdb7160e50c1c4b1d07013eb6104142f84f3f30d
tomas12py <tomas.martinez@polygon.us>
2025-06-12 23:52:57 -0500

    Settings for custom pagination were created

---
+++ app_aparments/pagination.py
@@ -0,0 +1,17 @@
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.exceptions import ValidationError
+from rest_framework.response import Response
+from rest_framework import status
+class CustomPagination(PageNumberPagination):
+    page_size = 4
+    page_size_query_param = "page_size"
+    max_page_size = 10
+    page_query_param = "page"
+
+    def get_page_size(self, request):
+        page_size = request.query_params.get(self.page_size_query_param)
+
+        if page_size and ( "'" in page_size or '"' in page_size ):
+            raise ValidationError("The value can't contain quotes")
+
+        return super().get_page_size(request)
