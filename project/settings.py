96099aada27137eaafcd5807bf5dbb03a042a108
tomas12py <tomas.martinez@polygon.us>
2025-06-14 00:13:40 -0500

    The settings for throttling was added

--- project/settings.py
+++ project/settings.py
@@ -138,7 +138,15 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
-    'DEFAULT_SCHEMA_CLASS':"drf_spectacular.openapi.AutoSchema"
+    'DEFAULT_SCHEMA_CLASS':"drf_spectacular.openapi.AutoSchema",
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle'
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon':'4/day',
+        'user': '6/day'
+    }
 }
 
 
