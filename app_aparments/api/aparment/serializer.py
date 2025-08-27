d9118444224bc5f9562985e82116a1ea69863e28
tomas12py <tomas.martinez@polygon.us>
2025-08-26 23:41:33 -0500

    The serializers name were changed

--- app_aparments/api/aparment/serializer.py
+++ app_aparments/api/aparment/serializer.py
@@ -1,21 +1,21 @@
-from app_aparments.models import Apartment,AparmentImage
+from app_aparments.models import Apartment,ApartmentImage
 from app_aparments.validators import is_letter_and_spaces
 from rest_framework import serializers
 
 
-class AparmentImageSerializer(serializers.ModelSerializer):
+class ApartmentImageSerializer(serializers.ModelSerializer):
 
     class Meta():
-        model = AparmentImage
+        model = ApartmentImage
         fields = '__all__'
 
 class ApartmentSerializer(serializers.ModelSerializer):
-    images = AparmentImageSerializer(many = True,read_only = True)
+    images = ApartmentImageSerializer(many = True,read_only = True)
     class Meta():
         model = Apartment
         fields = '__all__'
 
 
-class AparmentFilteringSerializer(serializers.Serializer):
+class ApartmentFilteringSerializer(serializers.Serializer):
     location = serializers.CharField(
         required=False, max_length=150, validators=[is_letter_and_spaces])
