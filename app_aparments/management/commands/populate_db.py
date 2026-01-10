11793fd00ff9b6c599585066e9aa7b4a54262843
tomas12py <tomas.martinez@polygon.us>
2026-01-09 21:40:32 -0500

    The structure for personalized commands has been created

---
+++ app_aparments/management/commands/populate_db.py
@@ -0,0 +1,108 @@
+from django.core.management.base import BaseCommand,CommandError
+from app_aparments.models import Apartment
+from django.db.utils import IntegrityError
+
+
+apartment_data = [
+   
+     {
+    "title": "",
+    "price": 100,
+    "rooms": 2,
+    "bathrooms": 0,
+    "address": "hola",
+    "location": "0",
+    "square_meters": 50.75,
+    "description": "Aparment for sell",
+    "garage": 0,
+    "elevator": False,
+    "pool": False,
+    "state": "rent"
+  },
+   {
+    "title": "second aparment",
+    "price": 100,
+    "rooms": 2,
+    "bathrooms": 0,
+    "address": "hola",
+    "location": "0",
+    "square_meters": 50.75,
+    "description": "Aparment for sell",
+    "garage": 0,
+    "elevator": False,
+    "pool": False,
+    "state": "rent"
+  },
+   {
+    "title": "third aparment",
+    "price": 100,
+    "rooms": 2,
+    "bathrooms": 0,
+    "address": "hola",
+    "location": "0",
+    "square_meters": 50.75,
+    "description": "Aparment for sell",
+    "garage": 0,
+    "elevator": False,
+    "pool": False,
+    "state": "rent"
+  },
+   {
+    "title": "fourth aparment",
+    "price": 100,
+    "rooms": 2,
+    "bathrooms": 0,
+    "address": "hola",
+    "location": "0",
+    "square_meters": 50.75,
+    "description": "Aparment for sell",
+    "garage": 0,
+    "elevator": False,
+    "pool": False,
+    "state": "rent"
+  },
+   {
+    "title": "seven aparment",
+    "price": 100,
+    "rooms": 2,
+    "bathrooms": 0,
+    "address": "hola",
+    "location": "0",
+    "square_meters": 50.75,
+    "description": "Aparment for sell",
+    "garage": 0,
+    "elevator": False,
+    "pool": False,
+    "state": "rent"
+  },
+
+]
+
+
+class Command(BaseCommand):
+
+    def handle(self,*args,**kwars):
+
+        try:
+            for data in apartment_data:
+                apartment = Apartment.objects.create(
+                    title = data["title"],
+                    price = data["price"],
+                    rooms = data["rooms"],
+                    bathrooms = data["bathrooms"],
+                    address = data["address"],
+                    location = data["location"],
+                    square_meters = data["square_meters"],
+                    description = data["description"],
+                    garage = data["garage"],
+                    elevator = data["elevator"],
+                    pool = data["pool"],
+                    state = data["state"]
+                )
+
+                apartment.save()
+
+                self.stdout.write(self.style.SUCCESS(f"The apartment with the id {apartment.id} and name {apartment.title} has been created"))
+
+        except IntegrityError as e:
+          raise CommandError((f"There was an error: {e}"))
