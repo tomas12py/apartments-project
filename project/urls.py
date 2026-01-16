557beabce90e848fdb17d95f51ef48c812b6336f
tomas12py <tomas.martinez@polygon.us>
2026-01-16 00:34:24 -0500

    The endpoints urls have been changed their nomeclature

--- project/urls.py
+++ project/urls.py
@@ -18,29 +18,28 @@ Including another URLconf
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenBlacklistView
 from app_aparments.api.healthcheck.views import Healthcheck
-from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import include, path
+from django.conf import settings
 from django.contrib import admin
 
 
 urlpatterns = [
     path('admin/clearcache/',include('clearcache.urls')),
     path('admin/', admin.site.urls),
-    path("login/", TokenObtainPairView.as_view()),
-    path("refresh-token/",TokenRefreshView.as_view()),  
-    path("blacklisting-token/",TokenBlacklistView.as_view()),
-    path("api/",include("app_aparments.api.user.urls")),
-    path("api/",include("app_aparments.api.aparment.urls")),
+    path("api/v1/login/", TokenObtainPairView.as_view()),
+    path("api/v1/tokens/refresh/",TokenRefreshView.as_view()),  
+    path("api/v1/blacklist-token/",TokenBlacklistView.as_view()),
+    path("api/v1/",include("app_aparments.api.user.urls")),
+    path("api/v1/",include("app_aparments.api.aparment.urls")),
     path("api/schema", SpectacularAPIView.as_view(), name='schema'),    
-    path("api/schema/swagger-ui/",
+    path("api/v1/schema/swagger-ui/",
          SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'), 
-    path("api/schema/redoc/",
+    path("api/v1/schema/redoc/",
          SpectacularRedocView.as_view(url_name='schema'), name="redoc"),
-    path('api/health/',Healthcheck.as_view())
+    path('api/v1/health/',Healthcheck.as_view()) 
+
 ]
 
 if settings.DEBUG:
     urlpatterns+= static(settings.STATIC_URL,document_root = settings.STATIC_ROOT)
-
-print(settings.STATIC_ROOT)
