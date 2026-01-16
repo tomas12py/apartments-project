d18d4d6d9aacb0aeac8b7edf78bd4a80535e08d0
tomas12py <tomas.martinez@polygon.us>
2026-01-16 00:32:38 -0500

    Unnecessary imports were deleted

--- app_aparments/api/healthcheck/views.py
+++ app_aparments/api/healthcheck/views.py
@@ -1,6 +1,4 @@
-from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
-from rest_framework.response import Response
 from health_check.views import MainView
 
 class Healthcheck(APIView, MainView):
